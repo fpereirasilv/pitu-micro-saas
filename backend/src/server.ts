e87367dd7d8ee3d3031a6a6abee4f16d49f519f2
unknown <fpereirasilv@gmail.com>
2020-11-25 15:35:05 -0300

    Commit inicial backend

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import database from './database';
+
+database.sync();
+console.log('Database running at 3306')
+
+app.listen(3000)
+console.log('Server conectado na porta 3000');
