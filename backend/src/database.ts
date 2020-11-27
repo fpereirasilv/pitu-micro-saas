81186833e18698b467b439ff9cf27f140f3a1dc8
unknown <fpereirasilv@gmail.com>
2020-11-27 09:25:37 -0300

    Ajuste das configuras o banco de dados

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,8 @@
 import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
 
-const sequelize = new Sequelize('mysql://doisdasorte01:fps171926@mysql.doisdasorte.kinghost.net:3306/doisdasorte01');
+dotenv.config();
+
+const sequelize = new Sequelize(`mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@mysql.doisdasorte.kinghost.net:3306/${process.env.DATABASE_NAME}`);
 
 export default sequelize;
